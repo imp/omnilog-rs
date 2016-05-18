1f19df59498111e265d4576af59b1a4b9a92101d
Cyril Plisko <cyril.plisko@mountall.com>
2016-05-18 18:51:20 +0300

    Simple skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,48 @@
+#[derive(Debug, Default)]
+pub struct Sink;
+
+#[derive(Debug, Default)]
+pub struct Filter;
+
+#[derive(Debug, Default)]
+pub struct Logger {
+    count: usize,
+    filters: Vec<Filter>,
+    sinks: Vec<Sink>,
+}
+
+impl Logger {
+    pub fn new() -> Self {
+        Logger {
+            count: 0,
+            filters: vec![],
+            sinks: vec![],
+        }
+    }
+
+    pub fn log(&mut self, msg: &str) {
+        let len = msg.len();
+
+        if len > 0 {
+            self.count += len;
+        }
+    }
+
+    pub fn count(&self) -> usize {
+        self.count
+    }
+}
+
+pub fn get_logger() -> Logger {
+    Logger::new()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
+    fn simple_get_logger() {
+        let log = get_logger();
+        assert_eq!(log.count(), 0);
     }
 }
