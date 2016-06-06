1c9768360737dc9fcb5c55b7357f44c759f38f17
Cyril Plisko <cyril.plisko@mountall.com>
2016-06-06 09:35:07 +0300

    Move Logger trait code into its own submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,14 @@
-mod logrecord;
+mod logger;
 mod loggers;
+mod logrecord;
 mod processor;
 mod loglevel;
 
-pub use logrecord::LogRecord;
-pub use processor::{Processor, NopProcessor};
+pub use logger::{Logger, console_logger, syslog_logger, get_logger};
 pub use loggers::{DefaultLogger, ConsoleLogger, SyslogLogger};
 pub use loglevel::LogLevel;
-
-pub trait Logger {
-    /// Submit log event to logger
-    fn log<'a>(&mut self, records: Vec<&'a LogRecord>) -> Vec<&'a LogRecord>;
-
-    // Usual implementation looks like this
-    // (assuming there is _log() method that does the actual logging work)
-    //
-    // fn log<'a>(&mut self, records: Vec<&'a LogRecord>) -> Vec<&'a LogRecord> {
-    //     let r = match self.next {
-    //         Some(ref mut next) => next.log(records),
-    //         None => records,
-    //     };
-    //     self._log(r)
-    // }
-
-    /// Create default logger
-    fn default_logger() -> DefaultLogger
-    {
-        DefaultLogger::new()
-    }
-
-    /// Chain console logger
-    fn console_logger(self, name: &str) -> ConsoleLogger<Self>
-        where Self: Sized + Logger
-    {
-        ConsoleLogger::chain(self).name(name)
-    }
-
-    /// Chain syslog logger
-    fn syslog_logger(self) -> SyslogLogger<Self>
-        where Self: Sized + Logger
-    {
-                SyslogLogger::chain(self)
-    }
-}
-
-/// Create console logger
-pub fn console_logger(name: &str) -> ConsoleLogger<DefaultLogger> {
-    ConsoleLogger::new().name(name)
-}
-
-/// Create new syslog logger
-pub fn syslog_logger() -> SyslogLogger<DefaultLogger> {
-    SyslogLogger::new()
-}
-
-/// Get an instance of a default logger.
-/// The exact kind of the default logger is not defined
-/// and happens to be a DefaultLogger at the moment.
-/// Good for a generic logging facilities and as an API demonstrator.
-pub fn get_logger() -> DefaultLogger {
-    DefaultLogger::new()
-}
+pub use logrecord::LogRecord;
+pub use processor::{Processor, NopProcessor};
 
 #[cfg(test)]
 mod tests {
---
+++ src/logger.rs
@@ -0,0 +1,56 @@
+use super::LogRecord;
+use super::{DefaultLogger, ConsoleLogger, SyslogLogger};
+
+pub trait Logger {
+    /// Submit log event to logger
+    fn log<'a>(&mut self, records: Vec<&'a LogRecord>) -> Vec<&'a LogRecord>;
+
+    // Usual implementation looks like this
+    // (assuming there is _log() method that does the actual logging work)
+    //
+    // fn log<'a>(&mut self, records: Vec<&'a LogRecord>) -> Vec<&'a LogRecord> {
+    //     let r = match self.next {
+    //         Some(ref mut next) => next.log(records),
+    //         None => records,
+    //     };
+    //     self._log(r)
+    // }
+
+    /// Create default logger
+    fn default_logger() -> DefaultLogger
+    {
+        DefaultLogger::new()
+    }
+
+    /// Chain console logger
+    fn console_logger(self, name: &str) -> ConsoleLogger<Self>
+        where Self: Sized + Logger
+    {
+        ConsoleLogger::chain(self).name(name)
+    }
+
+    /// Chain syslog logger
+    fn syslog_logger(self) -> SyslogLogger<Self>
+        where Self: Sized + Logger
+    {
+                SyslogLogger::chain(self)
+    }
+}
+
+/// Create console logger
+pub fn console_logger(name: &str) -> ConsoleLogger<DefaultLogger> {
+    ConsoleLogger::new().name(name)
+}
+
+/// Create new syslog logger
+pub fn syslog_logger() -> SyslogLogger<DefaultLogger> {
+    SyslogLogger::new()
+}
+
+/// Get an instance of a default logger.
+/// The exact kind of the default logger is not defined
+/// and happens to be a DefaultLogger at the moment.
+/// Good for a generic logging facilities and as an API demonstrator.
+pub fn get_logger() -> DefaultLogger {
+    DefaultLogger::new()
+}
